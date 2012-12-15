cd59ada5582250fad1ad63a2dbd7d6aa30b70292
numberty <numberty@163.com>
2012-12-15 22:32:01 +0800

    add proj
    Signed-off-by: numberty <numberty@163.com>

---
+++ src/numberty/git/JavaTest.java
@@ -0,0 +1,13 @@
+package numberty.git;
+
+public class JavaTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("git test");
+	}
+
+}
